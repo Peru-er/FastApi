e265bf41e5bd8b0da56ffc7e60ba694fc1022eb2
Peru-er <shynkar.snizhana@gmail.com>
2025-08-26 21:07:05 +0200

    db.py

---
+++ 27.08/ex. 2/db.py
@@ -0,0 +1,34 @@
+
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+target_metadata = Base.metadata
+
+
+class Animal(Base):
+    __tablename__ = "animals"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    age = Column(Integer, nullable=False)
+    adopted = Column(Boolean, default=False, nullable=False)
+    health_status = Column(String, default="healthy", nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
